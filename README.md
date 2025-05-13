ef2244e9511eea4dd5f94989b7be8c1248ecb6a9
kobbin003 <dkfeto@gmail.com>
2025-05-13 13:23:53 +0530

    counter class done

---
+++ README.md
@@ -0,0 +1,1 @@
+# Counter app(Class component) task from Crio's INTV-1 sprint session-2
---
+++ src/Counter.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+export class Counter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { count: 0 };
+	}
+
+	handleCounterChange(val) {
+		this.setState({ count: this.state.count + val });
+	}
+	// handleCounterChange = (val) => {
+	// 	this.setState({ count: this.state.count + val });
+	// };
+
+	// demoHandler = () => {
+	// 	console.log("demo clicked", this.state.count);
+	// };
+	demoHandler() {
+		this.setState({ count: 100 });
+		// console.log("demo clicked", this.state.count);
+	}
+
+	render() {
+		return (
+			<>
+				<h1>Counter App</h1>
+				<p>Count: {this.state.count}</p>
+				<div>
+					<button onClick={() => this.handleCounterChange(1)}>Increment</button>
+					<button onClick={() => this.handleCounterChange(-1)}>
+						Decrement
+					</button>
+					<button onClick={this.demoHandler}>demo</button>
+				</div>
+			</>
+		);
+	}
+}
